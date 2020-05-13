31d9c1718c46e2f0257ee14f50e79d853c91b6d3
David Alba-Lopez <albalopezdavid@gmail.com>
2020-05-13 16:14:24 -0400

    Initial Commit

---
+++ ImproseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImproseApp
+//
+//  Created by CSSE Department on 5/13/20.
+//  Copyright © 2020 Rose-Hulman CSSE484. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
